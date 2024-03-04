e45e6e11fd925a6ac78a6ba8d4805813033af977
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-03-04 16:58:41 -0500

    variables testing

---
+++ 01-vars/outputs.tf
@@ -0,0 +1,3 @@
+variable "sample_name" {
+  default = "hello world"
+}
---
+++ 01-vars/var.tf
@@ -0,0 +1,7 @@
+output "sample_name" {
+  value = var.sample_name
+}
+
+output "sample_name1" {
+  value = "value of variable_name = ${var.sample_name}"
+}
