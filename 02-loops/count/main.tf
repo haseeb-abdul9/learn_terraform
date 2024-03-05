d8d9296fbfdda2bb73e0dcc5785d2f0cc1c2c24f
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-03-04 23:12:45 -0500

    count loops

--- 02-loops/count/main.tf
+++ 02-loops/count/main.tf
@@ -2,14 +2,11 @@ resource "null_resource" "null" {
   count = 10
 }
 
-variable "list" {
-  default = ["apple", "banana", "mango"]
-}
-
 resource "null_resource" "list" {
   count = length(var.list)
 
-  provisioner "local-exec" {
-    command = "echo${var.list[count.index]}"
-  }
+}
+
+variable "list" {
+  default = ["apple", "banana", "mango"]
 }
