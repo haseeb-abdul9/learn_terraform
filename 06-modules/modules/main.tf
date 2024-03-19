286ca75f64936146d2450773a1b63947c8fe5577
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-03-19 15:35:14 -0400

    learning modules

--- 06-modules/modules/main.tf
+++ 06-modules/modules/main.tf
@@ -1,18 +1,6 @@
 resource "null_resource" "existing" {
   provisioner "local-exec" {
-    command = "echo hello world from module-input ${input}"
-  }
-}
-
-resource "null_resource" "nothing1" {
-  provisioner "local-exec" {
-    command = "echo hello world from module 1"
-  }
-}
-
-resource "null_resource" "nothing2" {
-  provisioner "local-exec" {
-    command = "echo hello world from module 2"
+    command = "echo hello world from module-input ${var.input}"
   }
 }
 
