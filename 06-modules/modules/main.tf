cdd2edded235224708e11fcfb42d5dcca596c927
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-03-19 15:29:26 -0400

    learning modules

--- 06-modules/modules/main.tf
+++ 06-modules/modules/main.tf
@@ -1,18 +1,18 @@
-resource "null_resource" "nothing" {
+resource "null_resource" "existing" {
   provisioner "local-exec" {
-    command = "echo hello world frpm mpdule-input ${input}"
+    command = "echo hello world from module-input ${input}"
   }
 }
 
 resource "null_resource" "nothing1" {
   provisioner "local-exec" {
-    command = "echo hello world frpm mpdule 1"
+    command = "echo hello world from module 1"
   }
 }
 
 resource "null_resource" "nothing2" {
   provisioner "local-exec" {
-    command = "echo hello world frpm mpdule 2"
+    command = "echo hello world from module 2"
   }
 }
 
