d360c81b7c6d1478df064df027fbc1ae4758633a
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-03-20 01:02:09 -0400

    learning modules

--- ex_2/ec2/main.tf
+++ ex_2/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.centOS.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.sg_id
+  vpc_security_group_ids = [var.sg_id]
 
   tags = {
     Name = "module"
